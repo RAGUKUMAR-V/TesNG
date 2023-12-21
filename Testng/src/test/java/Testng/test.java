b7245b112298c43aafa11cd0d30c4e13a001c9e1
Ragukumar <Ragukumar@LAPTOP-H8GD1N65>
2023-12-22 00:09:39 +0530

    lates

---
+++ Testng/src/test/java/Testng/test.java
@@ -0,0 +1,18 @@
+package Testng;
+
+public class test {
+
+	static String name="ragu";
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+		
+		String name=test.name.concat("kumar");
+		
+	}
+
+}
